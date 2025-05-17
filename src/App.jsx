31a74ce603842a2a3f8916fc9e018090c80cf83d
CarloErnestoPriego <aceytunocarlos897@hotmail.com>
2025-05-17 13:17:59 -0600

    Modificacion de archivos main y App e incorporacion de assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
+import Card from './Components/Card'
+import Button from './Components/Button'
+
+import spiderIcon from './assets/images/spiderman-icon.jpg'
+import valorantIcon from './assets/images/valorant-icon.png'
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Card
+      image= {spiderIcon}
+      username= 'Carlos'
+      postContent='Mi primera publicacion'
+      />
+      <Card
+      image= {valorantIcon  }
+      username= 'Valorant'
+      postContent='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      />
+      <Button
+      text='enviar'
+      />
     </>
   )
 }
